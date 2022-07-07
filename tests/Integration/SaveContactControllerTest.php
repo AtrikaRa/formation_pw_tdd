555690660a8ade51732a5919875fa5c836423b9e
AtrikaRa <arandriamanalina@passion-web.info>
2022-07-07 16:35:41 +0300

    test

--- tests/Integration/SaveContactControllerTest.php
+++ tests/Integration/SaveContactControllerTest.php
@@ -17,7 +17,7 @@ class SaveContactControllerTest extends WebTestCase
         $container = static::getContainer();
         $em = $container->get('doctrine.orm.entity_manager');
 
-        $this->assertEquals(true, true);
+        $this->assertEquals(true, false);
     }
 
     public function testSendEmailContactPage() {
